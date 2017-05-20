1344a14571fbd06a96d5f5a8c495b602c4a55a59
Raul Martinez i Peris <raul@cuchame.es>
2017-05-20 16:51:43 +0200

    Test all systems

--- prepare_project.sh
+++ prepare_project.sh
@@ -1,10 +1,18 @@
 #/bin/bash
 echo "Preparing project"
+echo "\nDetect if exist system folder"
 if [ ! -d "culturaaccesible-system" ]; then
   git clone https://github.com/devscola/culturaaccesible-system.git
+else
+echo "Ok."
 fi
 
+echo "\nDetect if exist app folder"
 if [ ! -d "culturaaccesible-app" ]; then
   git clone https://github.com/devscola/culturaaccesible-app.git
-  
+else
+echo "Ok."
 fi
+
+echo "\nClonning is update. You must run 'docker-compose up --build' and tests."
+
--- run_tests_system.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-cd /opt/app/culturaaccesible-system
-touch travis.ci
-bundle install
-bundle exec rake prepare
-bundle exec rake test
